02376cdf61ad100b700fe5a7f92b0865fd702b1f
Mara <mara-li@outlook.fr>
2023-01-20 13:47:50 +0100

    add changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+
+<a name="v1.0.0"></a>
+## [v1.0.0](https://github.com/ObsidianPublisher/follow_template/compare/0.0.2...v1.0.0) (0001-01-01)
+
+
+<a name="0.0.2"></a>
+## [0.0.2](https://github.com/ObsidianPublisher/follow_template/compare/0.0.1...0.0.2) (2023-01-20)
+
+
+<a name="0.0.1"></a>
+## 0.0.1 (2023-01-20)
+
---
+++ create_release.py
@@ -0,0 +1,25 @@
+import argparse
+import os
+from git import Repo
+
+
+def create_tag(tag_name: str):
+    repo = Repo(os.path.dirname(os.path.realpath(__file__)))
+    tag = repo.create_tag(tag_name, message=f"chore(bump): v{tag_name}")
+    print(f"Tag {tag_name} created")
+    # commit
+    repo.git.add(update=True)
+    repo.index.commit(f"chore(bump): v{tag_name}")
+    repo.remotes.origin.push(tag.path)
+
+def generate_changelog(version: str):
+    print(f"Generating changelog for version {version}")
+    os.system(f"git-chglog --next-tag v{version} --output CHANGELOG.md")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("version", help="Version to create tag for")
+    args = parser.parse_args()
+    generate_changelog(args.version)
+    create_tag(args.version)
